d829ef5ce99c77dcda4e2f927232f9aa8c5814d9
imo-tikuwa <imotikuwabathclean@gmail.com>
2023-11-28 21:48:22 +0900

    add 「publicDir: false」 to vite.config.ts
    - settings to avoid including unnecessary favicon.ico

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from 'node:path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  publicDir: false,
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
