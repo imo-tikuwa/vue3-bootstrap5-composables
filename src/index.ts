26ac0b019bef2301c8b1b5e906f22db0f917a900
imo-tikuwa <imotikuwabathclean@gmail.com>
2023-12-15 00:00:31 +0900

    edit build config, etc.
    - package.jsonのバージョン更新

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
+// confirm modal resources.
 import ConfirmModalDialog from './components/useConfirmModal/ConfirmModalDialog.vue'
 import ConfirmModalInjection from './components/useConfirmModal/ConfirmModalInjection.vue'
 import { useConfirmModal } from './composables/confirmModal'
 import { ConfirmModalPlugin } from './plugins/confirmModal'
 
-export { ConfirmModalDialog, ConfirmModalInjection, useConfirmModal, ConfirmModalPlugin }
+// composable toast resources.
+import ToastContainer from './components/useToast/ToastContainer.vue'
+import ToastDialog from './components/useToast/ToastDialog.vue'
+import { useToast } from './composables/toast'
+import { ComposableToastPlugin } from './plugins/composableToast'
+
+export {
+  ConfirmModalDialog,
+  ConfirmModalInjection,
+  useConfirmModal,
+  ConfirmModalPlugin,
+  ToastContainer,
+  ToastDialog,
+  useToast,
+  ComposableToastPlugin
+}
